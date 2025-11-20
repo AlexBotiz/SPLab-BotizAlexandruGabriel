45f8e80ba2a2e5c464367a1223a3dcc44a11dfe1
AlexBotiz <alexandru.botiz04@e-uvt.ro>
2025-11-20 14:20:06 +0200

    Lab3

---
+++ src/main/java/laborator/Author.java
@@ -0,0 +1,14 @@
+package laborator;
+
+public class Author {
+    String name;
+    String surname;
+    Author(){}
+    Author(String name, String surname){
+        this.name=name;
+        this.surname=surname;
+    }
+    public void print() {
+        System.out.println("Author name: " + name + "\n Author surname: " + surname);
+    }
+}
---
+++ src/main/java/laborator/ImageContent.java
@@ -0,0 +1,11 @@
+package laborator;
+
+public class ImageContent {
+    private String name;
+    public ImageContent(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+}
